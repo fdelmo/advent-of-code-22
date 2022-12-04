ccd47940c833b155260e38a79a598bf201543f19
Ferran <ferrandelmoral@gmail.com>
2022-12-04 13:34:29 +0100

    Problem4.2

--- problem4/problem4.py
+++ problem4/problem4.py
@@ -1,3 +1,5 @@
+import time
+
 
 def check_if_pair_is_contained(pair: str) -> bool:
     """
@@ -27,17 +29,39 @@ def check_if_pair_is_contained(pair: str) -> bool:
             return True
 
 
-def count_total_contained_pairs(pairs: list['str']) -> int:
+def check_if_overlap(pair: str) -> bool:
+    """
+    Given a pair of the type '2-4,3-5', return True if one of the segments
+    overlap. In this example the result should be True since 3-4 are 
+    contained in both segments.
+
+    # NOTE: We don't care what segments overlap, only if there is any that does.
+    """
+    seg1, seg2 = pair.split(',')
+
+    seg1 = tuple(map(int, seg1.split('-')))
+    seg2 = tuple(map(int, seg2.split('-')))
+
+    if (seg1[0] >= seg2[0]) & (seg1[0] <= seg2[1]):
+        return True
+    elif (seg1[1] >= seg2[0]) & (seg1[1] <= seg2[1]):
+        return True
+
+    return check_if_pair_is_contained(pair)
+
+
+def count_total_contained_and_overlaped_pairs(pairs: list['str']) -> int:
     """
     Given an input with multiple segment pairs, count how many pairs have
     one segment fully contained in the other
     """
 
-    acc = 0
+    contained = 0
+    overlaped = 0
     for pair in pairs:
-        acc += check_if_pair_is_contained(pair)
-
-    return acc
+        contained += check_if_pair_is_contained(pair)
+        overlaped += check_if_overlap(pair)
+    return contained, overlaped
 
 
 if __name__ == '__main__':
@@ -46,12 +70,12 @@ if __name__ == '__main__':
         pairs = [line.rstrip('\n') for line in input]
 
     test = [
-        '2-4,6-8',
-        '2-3,4-5',
+        # '2-4,6-8',
+        # '2-3,4-5',
         '5-7,7-9',
         '2-8,3-7',
         '6-6,4-6',
         '2-6,4-8'
     ]
 
-    print(count_total_contained_pairs(pairs))
+    print(count_total_contained_and_overlaped_pairs(pairs=pairs))
