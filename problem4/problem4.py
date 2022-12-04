8b651ab972fad81e4fe4cae511e0f55a2041382f
Ferran <ferrandelmoral@gmail.com>
2022-12-04 13:41:09 +0100

    Refactored the code a bit

--- problem4/problem4.py
+++ problem4/problem4.py
@@ -1,16 +1,12 @@
 import time
 
 
-def check_if_pair_is_contained(pair: str) -> bool:
+def check_if_pair_is_contained(seg1: tuple[int], seg2: tuple[int]) -> bool:
     """
     Given a pair of the type '2-3,2-5', return True if one of the segments
     fully contain the other. In this example the result should be True
     since 2-5 fully contain 2-3.
     """
-    seg1, seg2 = pair.split(',')
-
-    seg1 = tuple(map(int, seg1.split('-')))
-    seg2 = tuple(map(int, seg2.split('-')))
 
     seg1_range = seg1[-1]-seg1[0]
     seg2_range = seg2[-1]-seg2[0]
@@ -29,7 +25,7 @@ def check_if_pair_is_contained(pair: str) -> bool:
             return True
 
 
-def check_if_overlap(pair: str) -> bool:
+def check_if_overlap(seg1: tuple[int], seg2: tuple[int]) -> bool:
     """
     Given a pair of the type '2-4,3-5', return True if one of the segments
     overlap. In this example the result should be True since 3-4 are 
@@ -37,17 +33,13 @@ def check_if_overlap(pair: str) -> bool:
 
     # NOTE: We don't care what segments overlap, only if there is any that does.
     """
-    seg1, seg2 = pair.split(',')
-
-    seg1 = tuple(map(int, seg1.split('-')))
-    seg2 = tuple(map(int, seg2.split('-')))
 
     if (seg1[0] >= seg2[0]) & (seg1[0] <= seg2[1]):
         return True
     elif (seg1[1] >= seg2[0]) & (seg1[1] <= seg2[1]):
         return True
 
-    return check_if_pair_is_contained(pair)
+    return check_if_pair_is_contained(seg1, seg2)
 
 
 def count_total_contained_and_overlaped_pairs(pairs: list['str']) -> int:
@@ -59,8 +51,14 @@ def count_total_contained_and_overlaped_pairs(pairs: list['str']) -> int:
     contained = 0
     overlaped = 0
     for pair in pairs:
-        contained += check_if_pair_is_contained(pair)
-        overlaped += check_if_overlap(pair)
+        # separate the pair and transfor the str into int
+        seg1, seg2 = pair.split(',')
+        seg1 = tuple(map(int, seg1.split('-')))
+        seg2 = tuple(map(int, seg2.split('-')))
+
+        # compute the problem answers
+        contained += check_if_pair_is_contained(seg1, seg2)
+        overlaped += check_if_overlap(seg1, seg2)
     return contained, overlaped
 
 
