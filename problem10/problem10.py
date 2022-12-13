25069fc5c221e2602adaa0da67363309823e29bf
Ferran <ferrandelmoral@gmail.com>
2022-12-13 20:24:37 +0100

    Problem 10.2

--- problem10/problem10.py
+++ problem10/problem10.py
@@ -30,8 +30,40 @@ def sum_signal_strengths(register_vals: list[int], cycles: list[int]) -> int:
     return strength
 
 
+def determine_draw_pixel(register_value: int, sprite_position: int) -> str:
+    """
+    Given the sprite position at a certain cycle and the register value at
+    that cycle, determine if the sprite should draw the pixel or leave it blank.
+    """
+    return ("#" if (sprite_position in range(register_value-1, register_value + 2)) else ".")
+
+
+def draw_screen(register_values: list[int]) -> list[str]:
+    """
+    Given the value of the register X at each cycle, return a list of strings
+    (the screen rendered) with values "#" if the pixel is drawn or "." if it is not.
+    """
+    screen = [[]]
+    j = 0
+    i = 0
+    for value in register_values:
+        pixel = determine_draw_pixel(value, i)
+        screen[j].append(pixel)
+        i += 1
+        if (i % 40) == 0:
+            screen.append([])
+            j += 1
+            i = 0
+
+    screen.pop()
+    return screen
+
+
 with open('problem10/input10.txt', 'r') as input_file:
     lines = [line.rstrip('\n') for line in input_file]
 
     registers = calculate_register_in_cycle(lines)
     print(sum_signal_strengths(registers, [20, 60, 100, 140, 180, 220]))
+
+    screen_render = draw_screen(register_values=registers)
+    print(*screen_render, sep='\n')
